19f072dd99502069afc3c06adb9f73b3c28e4f68
aniruddhap19 <aniruddh3pillai@gmail.com>
2023-10-08 17:22:52 +0530

    break point

--- driver_proc.c
+++ driver_proc.c
@@ -9,17 +9,17 @@
 #include <linux/err.h>
 #include <linux/delay.h>
 #include <linux/uaccess.h>
-#define device_name "driver_test"
-
+#define out "out"
+#define in "in"
 static int device_open(struct inode *,struct file *);
 static ssize_t device_write(struct file *,const char *, size_t, loff_t *);
 static ssize_t device_read(struct file *,char *, size_t, loff_t *);
 static int device_release(struct inode *inode, struct file *file);
 static int gpio_label_get(void);
 static int gpio_initi(void);
-static int major;
 dev_t dev = 0;
 static struct class *dev_class;
+static struct cdev etx_cdev;
 static const char gpio_label_c[28][10] = {{"ID_SDA"},{"ID_SCL"},{"SDA1"},{"SCL1"},{"GPIO_GCLK"},{"GPIO5"},{"GPIO6"},
 {"SPI_CE1_N"},{"SPI_CE0_N"},{"SPI_MISO"},{"SPI_SCLK"},{"GPIO12"},{"GPIO12"},{"TXD1"},{"RXD1"},
 {"GPIO16"},{"GPIO17"},{"GPIO18"},{"GPIO19"},{"GPIO20"},{"GPIO21"},{"GPIO22"},{"GPIO23"},
@@ -44,26 +44,35 @@ static struct gpio_data{
 
 static struct gpio_data un1;
 
-static int kern_atoi(char* arr){
-	int index=0;
-	int arr1[3];
-	int sum=0;
-	while(*(arr+index)!='\0'){
-		index++;
+static int val_check(char* str){
+	int val = (int)str[0];
+	switch(val){
+		case 48:
+			return 0;
+			break;
+		case 49:
+			return 1;
+			break;
+		default:
+			return -1;
 	}
-	for(int i=0;i<index;i++){
-		arr1[i]=(int)arr[i]-48;
-	}
-	int c=(index-1);
-	for(int i=0;i<index;i++){
-		int temp=arr1[i];
-		for(int j=c;j>0;j--){
-			temp=temp*10;
-		}
-		sum=sum+temp;
-		c--;
-	}
-	return sum;
+
+}
+
+
+static int kern_atoi(char* str){
+    int i;
+    int result;
+    for(i =0;str[i] != '\0';i++){
+        if(i == 0){
+            result = (str[0]-0x30);
+        }else{
+            result =result*10;
+            result = result + (str[i]-0x30);
+        }
+    }
+    return result;
+
 }
 
 static int gpio_label_get(){
@@ -77,31 +86,32 @@ static int gpio_label_get(){
 
 static int gpio_initi(){
 	if(gpio_is_valid(un1.gpio_pin)==0){
-		printk(KERN_ALERT " GPIO pin not available");
+		printk(KERN_ALERT " GPIO pin not available\n");
 		return -1;
 	}
 	if(gpio_request(un1.gpio_pin,un1.gpio_nm)<0){
-		printk(KERN_ALERT "GPIO pin cannot be requested at the moment");
+		printk(KERN_ALERT "GPIO pin cannot be requested at the moment\n");
 		gpio_free(un1.gpio_pin);
 		return -2;
 	}
 	if(gpio_export(un1.gpio_pin,true)!=0){
-		printk(KERN_ALERT "Export of gpio failed");
+		printk(KERN_ALERT "Export of gpio failed\n");
 		return -3;
 		gpio_free(un1.gpio_pin);
 	}
 	if(strncmp(un1.dir,"in",4)==0){
 		if(gpio_direction_input(un1.gpio_pin)!=0){
-			printk(KERN_ALERT "Direction set as input failed");
+			printk(KERN_ALERT "Direction set as input failed\n");
 			gpio_free(un1.gpio_pin);
 			return -1;}
 	}
 	if(strncmp(un1.dir,"out",4)==0){
 		if(gpio_direction_output(un1.gpio_pin,un1.gpio_val)!=0){
-			printk(KERN_ALERT "Direction set as output failed");
+			printk(KERN_ALERT "Direction set as output failed\n");
 			gpio_free(un1.gpio_pin);
 			return -1;}
 	}
+	printk(KERN_INFO "gpio pin %d set as %s with value as %d \n",un1.gpio_pin,un1.dir,un1.gpio_val);
 	return 0;
 }
 
@@ -160,58 +170,74 @@ static ssize_t device_write(struct file* filp,const char __user *buffer,
 			break;
 		}
 	}
+	printk(KERN_INFO"GPIO selected is %d the dir is %s and val is %d",kern_atoi(p1),p3,val_check(p4));
 	if(kern_atoi(p1)>27){
 		printk(KERN_ALERT "GPIO pin out of scope\n");
 		return -2;
 	}
-	if((kern_atoi(p4)>1)||(strncmp(p3,"out",4)!=0)||(strncmp(p3,"in",4)!=0)||(kern_atoi(p4)<0)){
+	if((val_check(p4)==-1)||((strncmp(p3,out,4)!=0)&&(strncmp(p3,in,4)!=0))){
 		printk(KERN_ALERT "Invalid command\n");
 		return -3;
 	}
 
-	if(strncpy(un1.dir,p3,4)!=p3){
-		printk(KERN_ALERT "Failed to copy string");
-		return -2;
-	}
+	strncpy(un1.dir,p3,4);
 	un1.gpio_pin=kern_atoi(p1);
-	un1.gpio_val=kern_atoi(p4);
-	if(gpio_label_get != 0){
+	un1.gpio_val=val_check(p4);
+	if(gpio_label_get() != 0){
 		printk(KERN_ALERT "GPIO Label not obtained\n");
 		return -3;
 	}
-	if(gpio_initi() != 0){
+	volatile int tempo = gpio_initi();
+	if(tempo != 0){
 		printk(KERN_ALERT "GPIO PIN not accessable\n");
 		return -3;
 	}
+	
 	return 0;
 
 }
 
 
 static int __init hello(void){
-	major = register_chrdev(0,device_name,&fops);
-	if(major<0){
-		printk(KERN_ALERT "Device registration failed with error code %d\n",major);
-		return major;
+	if((alloc_chrdev_region(&dev,0,1,"etx_Dev"))<0){
+	pr_err("cannot allocate major number\n");
+	goto r_unreg;
 	}
-	dev_class = class_create(THIS_MODULE,"etx_class");
-	if(IS_ERR(dev_class)){
-		printk(KERN_ALERT "Cannot create the struct class for device\n");
-		class_destroy(dev_class);
-		unregister_chrdev(major,device_name);
-		goto err;
+	pr_info("Major = %d minor = %d \n");
+	
+	cdev_init(&etx_cdev,&fops);
+
+	if((cdev_add(&etx_cdev,dev,1)) < 0){
+		pr_err("Cannot add the device to the system\n");
+		goto r_del;
 	}
+
+
+	if(IS_ERR(dev_class = class_create(THIS_MODULE,"etx_class"))){
+		pr_err("Cannot create the struct class\n");
+		goto r_class;
+
+	}
+
+
 	if(IS_ERR(device_create(dev_class,NULL,dev,NULL,"etx_device"))){
-		printk(KERN_ALERT "Cannot create the device\n");
-		class_destroy(dev_class);
-		unregister_chrdev(major,device_name);
-		goto err;
+	pr_err("Cannot create the Device \n");
+	goto r_device;
 	}
-	printk(KERN_INFO "Module loaded sucessfully with major number %d\n",major);
-	printk(KERN_INFO "With device name %s\n",device_name);
+	
+	pr_info("Device Driver Insert...Done\n");
 	return 0;
-	err:
+	
+	r_device:
+	 device_destroy(dev_class,dev);
+	r_class:
+	 class_destroy(dev_class);
+	r_del:
+	 cdev_del(&etx_cdev);
+	r_unreg:
+	 unregister_chrdev_region(dev,1);
 	return -1;
+
 }
 
 
@@ -220,7 +246,8 @@ static void __exit bye(void){
 	gpio_free(un1.gpio_pin);
 	device_destroy(dev_class,dev);
 	class_destroy(dev_class);
-	unregister_chrdev(major,device_name);
+	cdev_del(&etx_cdev);
+	unregister_chrdev_region(dev,1);
 	printk(KERN_INFO "Module has been deloaded sucessfully");
 }
 
